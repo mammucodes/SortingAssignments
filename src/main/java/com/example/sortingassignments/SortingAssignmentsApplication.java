14219f6275630bfe7b7463bc724ade670f7bfc22
Mamatha Pudukutapu <mammucodes@gmail.com>
2024-12-04 14:30:38 -0600

    checking in java files for bubble sort

---
+++ src/main/java/com/example/sortingassignments/SortingAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sortingassignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAssignmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortingAssignmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sortingassignments/SortingAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sortingassignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAssignmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
